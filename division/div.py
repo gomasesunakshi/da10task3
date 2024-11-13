cacb6430f659364fda934735dc0d7340652268f9
gomasesunakshi <gomasesunakshi@gmail.com>
2024-11-13 23:34:27 +0530

    sunakshigomase :class method invoke

--- division/div.py
+++ division/div.py
@@ -2,6 +2,6 @@ class secondclass:
     val1 = 0
     val2 = 0
 
-print('secondclass',secondclass)
-print('secondclass.val1',secondclass.val1)
-print('secondclass.val2',secondclass.val2)
+f1 =secondclass
+print('f1.val1 = ',f1.val1)
+print('f1.val2 = ',f1.val2)
