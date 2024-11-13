a8080281fa392296c747ac913e53e50f5677c431
gomasesunakshi <gomasesunakshi@gmail.com>
2024-11-14 00:01:28 +0530

    sunakshigomase: call constructor with divide function

--- division/div.py
+++ division/div.py
@@ -2,6 +2,21 @@ class secondclass:
     val1 = 0
     val2 = 0
 
-f1 =secondclass
+
+    def __init__(self,value1,value2):
+        print('this is constructor')
+        self.val1 = value1
+        self.val2 = value2
+    
+    def dividevalue(self):
+        print(f'division={self.val1 /self.val2}')
+
+f1 = secondclass(8,9)
+
+
+f1.dividevalue()
 print('f1.val1 = ',f1.val1)
-print('f1.val2 = ',f1.val2)
+print(f'f1.val2 = {f1.val2}')
+
+f2 = secondclass(20,4)
+f2.dividevalue()
