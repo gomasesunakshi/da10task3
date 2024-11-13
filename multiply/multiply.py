16ff56d96d46ca985396d93385224db9cd9ddc43
gomasesunakshi <gomasesunakshi@gmail.com>
2024-11-14 00:25:18 +0530

    sunakshigomase: call const with divide function

--- multiply/multiply.py
+++ multiply/multiply.py
@@ -2,7 +2,21 @@ class firstclass:
     value1 =0
     value2 =0
 
+    def __init__(self,a,b):
+        print('this is constructor')
+        self.value1 = a
+        self.value2 = b
+        
 
-print('firstclass',firstclass)
-print('firstclass.value1',firstclass.value1)
-print('firstclass.value2',firstclass.value2)
+    def mulvalue(self):
+        print(f'multiply={self.value1*self.value2}')
+
+f1 =firstclass(4,6)
+
+f1.mulvalue()
+
+print('f1.val1  =', f1.value1)
+print(f'f1.val2 = {f1.value2}')
+
+f2 =firstclass(6,1)
+f2.mulvalue()
